b28698761221ac58b6fe7587a5b83a2edba81cce
starlikeleo <73845797+starlikeleo@users.noreply.github.com>
2021-02-24 19:19:09 +0900

    first_commit

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    binding.pry
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+
+end
