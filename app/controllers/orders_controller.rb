ae7af8e85b7b6c4ed2496201f3090a4acb029dd2
starlikeleo <73845797+starlikeleo@users.noreply.github.com>
2021-02-26 22:32:15 +0900

    forth_commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,7 +22,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_af53fb9296f56ab92890e7dd"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: order_params[:price],
       card: order_params[:token],
